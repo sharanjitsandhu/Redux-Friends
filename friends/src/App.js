e84d817306f09505d291179d37350bf1c33ccf2b
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-04-11 15:29:16 -0700

    Added a route for a Login Page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
+import Protected from "./components/Protected";
+import PrivateRoute from "./components/PrivateRoute";
 
+import "./App.css";
+
+// STEP I - Wrap everything inside Router. Add a Public route, a login route
+// and a protected route
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <ul>
+            <li>
+              <Link to="/public">Public Page</Link>
+            </li>
+            <li>
+              <Link to="/protected">Protected Page</Link>
+            </li>
+          </ul>
+          <h2>Friends(Redux App)</h2>
+          <Route path="/login" component={Login} />
+          <PrivateRoute exact path="/protected" component={Protected} />
+        </div>
+      </Router>
     );
   }
 }
